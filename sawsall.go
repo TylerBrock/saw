eaf3d1c30af11162f099d77b355b4a3694419348
Tyler Brock <tyler.brock@gmail.com>
2017-08-07 10:05:49 -0700

    in prog

--- sawsall.go
+++ sawsall.go
@@ -1,78 +1,135 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"time"
-
-	prettyjson "github.com/TylerBrock/go-prettyjson"
-	color "github.com/fatih/color"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/fatih/color"
 )
 
-func printLogs() {
+type jsonLog map[string]interface{}
 
+type formatter struct {
+	KeyColor        *color.Color
+	StringColor     *color.Color
+	BoolColor       *color.Color
+	NumberColor     *color.Color
+	NullColor       *color.Color
+	StringMaxLength int
+	DisabledColor   bool
+	Indent          int
+}
+
+var fmter = formatter{
+	KeyColor:        color.New(color.FgWhite),
+	StringColor:     color.New(color.FgGreen),
+	BoolColor:       color.New(color.FgYellow),
+	NumberColor:     color.New(color.FgCyan),
+	NullColor:       color.New(color.FgMagenta),
+	StringMaxLength: 0,
+	DisabledColor:   false,
+	Indent:          0,
+}
+
+func (jl *jsonLog) MarshalJSON() ([]byte, error) {
+	buffer := bytes.Buffer{}
+	var next string
+	for _, v := range *jl {
+		switch v.(type) {
+		case string:
+			next = fmter.StringColor.SprintFunc()(v)
+		case map[string]interface{}:
+			next = "{}"
+		case float64:
+			next = "1"
+		case bool:
+			next = "true"
+		case nil:
+			next = "null"
+		case []interface{}:
+			next = "[]"
+		default:
+			next = "XXX"
+		}
+		fmt.Println("next:", next, "val", v)
+		buffer.WriteString(next)
+	}
+	return buffer.Bytes(), nil
+}
+
+func handlePage(page *cloudwatchlogs.FilterLogEventsOutput, lastPage bool) bool {
+	//formatter := prettyjson.NewFormatter()
+	red := color.New(color.FgRed).SprintFunc()
+	white := color.New(color.FgWhite).SprintFunc()
+
+	for _, event := range page.Events {
+		str := aws.StringValue(event.Message)
+		bytes := []byte(str)
+		date := aws.MillisecondsTimeValue(event.IngestionTime)
+		dateStr := date.Format(time.RFC3339)
+		streamStr := aws.StringValue(event.LogStreamName)
+		jl := jsonLog{}
+		if err := json.Unmarshal(bytes, &jl); err != nil {
+			fmt.Printf("[%s] (%s) %s\n", red(dateStr), white(streamStr), str)
+		} else {
+			prettyStr, _ := json.Marshal(&jl)
+			fmt.Printf("[%s] (%s) %s\n", red(dateStr), white(streamStr), prettyStr)
+		}
+	}
+	return !lastPage
 }
 
 func main() {
-	// TODO: use StringVar to store directly into DescribeLogStreamsInput struct
 	logGroupName := flag.String("logGroup", "", "the log group to stream")
-	logStreamPrefix := flag.String("prefix", "", "the log stream prefix")
+	logStreams := flag.String("logStreams", "", "the log stream prefix")
+	noColor := flag.Bool("no-color", false, "Disable color output")
+
 	flag.Parse()
+
+	if *logGroupName == "" {
+		fmt.Println("Error: Must provide a logGroup!")
+		os.Exit(1)
+	}
+
+	if *noColor {
+		color.NoColor = true // disables colorized output
+	}
 
 	config := aws.Config{
 		Region: aws.String(endpoints.UsEast1RegionID),
 	}
 	sess := session.Must(session.NewSession(&config))
 	cw := cloudwatchlogs.New(sess)
-	lsd := cloudwatchlogs.DescribeLogStreamsInput{}
-	lsd.SetLogGroupName(*logGroupName)
-	if *logStreamPrefix != "" {
-		lsd.SetLogStreamNamePrefix(*logStreamPrefix)
-	} else {
-		lsd.SetOrderBy("LastEventTime")
-		lsd.SetDescending(true)
-	}
-	lsd.SetLimit(10)
-	logStreamsDescriptions, err := cw.DescribeLogStreams(&lsd)
-	if err != nil {
-		fmt.Println("error", err)
-	}
-	for _, stream := range logStreamsDescriptions.LogStreams {
-		name := aws.StringValue(stream.LogStreamName)
-		firstEvent := aws.MillisecondsTimeValue(stream.FirstEventTimestamp)
-		lastEvent := aws.MillisecondsTimeValue(stream.LastEventTimestamp)
-		storedBytes := aws.Int64Value(stream.StoredBytes)
-		fmt.Println(name, firstEvent, lastEvent, storedBytes)
-	}
-	stream := logStreamsDescriptions.LogStreams[0]
-	gle := cloudwatchlogs.GetLogEventsInput{}
-	gle.SetLogGroupName("production")
-	gle.SetLogStreamName(aws.StringValue(stream.LogStreamName))
-	gle.SetStartTime(aws.Int64Value(stream.FirstEventTimestamp))
-	gle.SetEndTime(aws.Int64Value(stream.LastEventTimestamp))
-	logEvents, err := cw.GetLogEvents(&gle)
-	if err != nil {
-		fmt.Println("error", err)
-	}
-	formatter := prettyjson.NewFormatter()
-	var dat map[string]interface{}
-	red := color.New(color.FgRed).SprintFunc()
-	for _, event := range logEvents.Events {
-		str := aws.StringValue(event.Message)
-		byt := []byte(str)
-		date := aws.MillisecondsTimeValue(event.IngestionTime)
-		dateStr := date.Format(time.RFC3339)
-		if err := json.Unmarshal(byt, &dat); err != nil {
-			fmt.Printf("[%s] %s\n", red(dateStr), str)
-		} else {
-			s, _ := formatter.Marshal(dat)
-			fmt.Printf("[%s] %s\n", red(dateStr), string(s))
+
+	input := cloudwatchlogs.FilterLogEventsInput{}
+	input.SetInterleaved(true)
+	input.SetLogGroupName(*logGroupName)
+	input.SetStartTime(aws.TimeUnixMilli(time.Now().Add(-10 * time.Second)))
+
+	if len(*logStreams) > 0 {
+		streams := strings.Split(*logStreams, ",")
+		streamNamePointers := make([]*string, len(streams))
+		for i, stream := range streams {
+			streamNamePointers[i] = &stream
 		}
+		input.SetLogStreamNames(streamNamePointers)
+	}
+
+	for {
+		err := cw.FilterLogEventsPages(&input, handlePage)
+		if err != nil {
+			fmt.Println("Error", err)
+			os.Exit(2)
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
