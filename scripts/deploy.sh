d4d3be551ed04054a4b8601fc03b3a844354f42d
Tyler Brock <tyler.brock@gmail.com>
2017-09-15 23:26:43 -0700

    Fix ldflags in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 go get github.com/mitchellh/gox
 go get github.com/tcnksm/ghr
-gox -ldflags "-X main.Version $BUILD_VERSION -X main.BuildDate $BUILD_DATE" -output "dist/saw_{{.OS}}_{{.Arch}}"
+gox -ldflags "-X main.Version=$BUILD_VERSION -X main.BuildDate=$BUILD_DATE" -output "dist/saw_{{.OS}}_{{.Arch}}"
 ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace `git describe --tags` dist/
