d85d3a36fb299506c84e723a8a3aa60b2bdc1bb0
Andrew Page <andrew@andrewpage.me>
2018-12-11 17:21:48 -0800

    Test cover existing time parse functionality

---
+++ config/configuration_test.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+  "testing"
+  "time"
+)
+
+var testTimeNow = time.Date(2018, 12, 01, 15, 30, 0, 0, time.UTC)
+
+// test the relative parsing functionality of getTime
+func TestGetTimeRelative(t *testing.T) {
+  relativeTest1, err := getTime("-2h", testTimeNow)
+
+  if err != nil || relativeTest1.Hour() != 13 {
+    t.Errorf("Expected relativeTest1 to set time back by two hours. Error: %s", err)
+  }
+
+  relativeTest2, err := getTime("-1h15m", testTimeNow)
+  if err != nil || !(relativeTest2.Hour() == 14 && relativeTest2.Minute() == 15) {
+    t.Errorf("Expected relativeTest2 to set time back by 1 hour and 15 minutes. Error: %s", err)
+  }
+}
+
+// test the absolute RFC3339 parsing functionality of getTime
+func TestGetTimeAbsoluteRFC3339(t *testing.T) {
+  absoluteTime1, err := getTime("2006-01-02T15:04:05+07:00", testTimeNow)
+
+  if err != nil && absoluteTime1.Hour() == 15 && absoluteTime1.Minute() == 4 {
+    t.Errorf("Expected to parse absolute time. Error: %s", err)
+  }
+}
