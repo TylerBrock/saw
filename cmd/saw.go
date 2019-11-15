4cb62ea939e97960e2622b417d4ec7de0b37bca4
Jean Mertz <jean@mertz.fm>
2019-11-15 23:08:49 +0100

    support multi-group log commands
    With this change, the first argument to the command is parsed as a
    regular expression and matched against the list of available log groups.
    For each matched log group, the relevant command is executed in
    parallel.
    
    The following commands support this pattern:
    
    * `get`
    * `streams`
    * `watch`
    
    The log group name is added to the formatted log output, to distinguish
    logs from different log groups.
    
    If you want to get an idential result as what you got before this
    commit, you would have to wrap your group name in regular expression
    begin and end tokens, meaning this:
    
        saw stream "my-log-group"
    
    Must now be written as:
    
        saw stream "^my-log-group$"
    
    Signed-off-by: Jean Mertz <jean@mertz.fm>

--- cmd/saw.go
+++ cmd/saw.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"regexp"
+	"sync"
+
+	"github.com/TylerBrock/saw/blade"
 	"github.com/TylerBrock/saw/config"
 	"github.com/spf13/cobra"
 )
@@ -28,4 +33,30 @@ func init() {
 	SawCommand.AddCommand(getCommand)
 	SawCommand.PersistentFlags().StringVar(&awsConfig.Region, "region", "", "override profile AWS region")
 	SawCommand.PersistentFlags().StringVar(&awsConfig.Profile, "profile", "", "override default AWS profile")
+}
+
+func runMultiGroup(pattern string, fn func(string)) error {
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return fmt.Errorf("invalid <log group> pattern: %s", err)
+	}
+
+	b := blade.NewBlade(&config.Configuration{}, &awsConfig, nil)
+
+	var wg sync.WaitGroup
+	for _, group := range b.GetLogGroups() {
+		name := group.LogGroupName
+		if name == nil || !re.MatchString(*name) {
+			continue
+		}
+
+		wg.Add(1)
+		go func(group string) {
+			defer wg.Done()
+			fn(group)
+		}(*name)
+	}
+
+	wg.Wait()
+	return nil
 }
